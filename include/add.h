e1639cc30f5a9f2d176fb7c8d28d4118fa4f8461
Anastasiaa Senina <miss.s-anas.itmm@yandex.ru>
2016-12-04 18:20:43 +0400

    added tests and implementation of fanction

---
+++ include/add.h
@@ -0,0 +1,8 @@
+struct CNode {
+  int data;
+  CNode *next;
+};
+int ReverseNumber(int);
+char* CommonPrefix(char**, int, int);
+void ReversePairElement(CNode *);
+void SearchElemEqSum(int&, int& , int,int*, int);
